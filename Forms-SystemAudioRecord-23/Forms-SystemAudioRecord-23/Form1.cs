131b5a8520b4fe5efef5ce37ec2d24d94788dae3
kietpham <kietfriendly@gmail.com>
2025-07-18 18:55:18 +0700

    Update Form1.cs

--- Forms-SystemAudioRecord-23/Forms-SystemAudioRecord-23/Form1.cs
+++ Forms-SystemAudioRecord-23/Forms-SystemAudioRecord-23/Form1.cs
@@ -12,11 +12,15 @@ namespace Forms_SystemAudioRecord_23
 {
     public partial class FormRecordSpeaker : Form
     {
-        private string outputFileName;
+        private string outputMicRecordFileName;
+        private string outputSystemRecordFileName;
         private WasapiLoopbackCapture capture;
         private WaveInEvent waveIn;
         private Thread micRecordThread;
-        private int threadControl; // 1 start, 2 stop
+        private Thread systemAudioRecordThread;
+        private int threadMicRecordControl; // 1 start, 2 stop
+        private int threadSystemAudioRecordControl; // 1 start, 2 stop
+
         public FormRecordSpeaker()
         {
             InitializeComponent();
@@ -33,9 +37,9 @@ namespace Forms_SystemAudioRecord_23
         void RecordMicIn()
         {
             int i = 0;
-            while (true && threadControl == 1)
+            while (threadMicRecordControl == 1 && i < 200)
             {
-                string fileOutputName = this.outputFileName + i.ToString() + ".wav";
+                string fileOutputName = this.outputMicRecordFileName + i.ToString() + ".wav";
                 var waveFormat = new WaveFormat(44100, 1);
                 using (var waveFile = new WaveFileWriter(fileOutputName, waveFormat))
                 {
@@ -52,10 +56,41 @@ namespace Forms_SystemAudioRecord_23
                         waveIn.StartRecording();
                         Thread.Sleep(5000);
                         this.waveIn.StopRecording();
-                        if (this.threadControl == 2) return;
+                        if (this.threadMicRecordControl == 2) return;
                         i++;
                     }
                 }
+            }
+        }
+
+        void RecordSystemAudio()
+        {
+            int i = 0;
+            while (threadSystemAudioRecordControl == 1 && i < 200)
+            {
+                string fileOutputName = this.outputSystemRecordFileName + i.ToString() + ".wav";
+                Console.WriteLine(fileOutputName);
+                this.capture = new WasapiLoopbackCapture();
+                var writer = new WaveFileWriter(fileOutputName, capture.WaveFormat);
+                capture.DataAvailable += (s, e_a) =>
+                {
+                    if (writer != null)
+                    {
+                        writer.WriteAsync(e_a.Buffer, 0, e_a.BytesRecorded);
+                        writer.FlushAsync();
+                    }
+                };
+                this.capture.StartRecording();
+                Thread.Sleep(5000);
+                this.capture.StopRecording();
+                if (this.threadSystemAudioRecordControl == 2)
+                {
+                    Console.WriteLine("RecordSystemAudio End");
+                    writer.Dispose();
+                    capture.Dispose();
+                    return;
+                }
+                i++;
             }
         }
 
@@ -67,42 +102,25 @@ namespace Forms_SystemAudioRecord_23
         private void button_Record_Click(object sender, EventArgs e)
         {
             var dialog = new SaveFileDialog();
-            dialog.Filter = "Wave files | *.wav";
+            dialog.Filter = "Wave files | *.part.";
             if (dialog.ShowDialog() != DialogResult.OK) return;
-            outputFileName = dialog.FileName;
+            outputSystemRecordFileName = dialog.FileName;
             button_Record.Enabled = false;
             buttonStop.Enabled = true;
-            capture = new WasapiLoopbackCapture();
-            var writer = new WaveFileWriter(outputFileName, capture.WaveFormat);
-            capture.DataAvailable += async (s, e_a) =>
-            {
-                if (writer != null)
-                {
-                    await writer.WriteAsync(e_a.Buffer, 0, e_a.BytesRecorded);
-                    await writer.FlushAsync();
-                }
-            };
-            capture.RecordingStopped += (s, e_s) =>
-            {
-                if (writer != null)
-                {
-                    writer.Dispose();
-                    writer = null;
-                }
-                button_Record.Enabled = true;
-                capture.Dispose();
-            };
-            capture.StartRecording();
+            this.threadSystemAudioRecordControl = 1;
+            this.systemAudioRecordThread = new Thread(new ThreadStart(RecordSystemAudio));
+            this.systemAudioRecordThread.Start();
         }
 
         private void buttonStop_Click(object sender, EventArgs e)
         {
             buttonStop.Enabled = false;
-            capture.StopRecording();
-            if (outputFileName == null) return;
+            button_Record.Enabled = true;
+            this.threadSystemAudioRecordControl = 2;
+            if (outputSystemRecordFileName == null) return;
             var processStartInfo = new ProcessStartInfo
             {
-                FileName = Path.GetDirectoryName(outputFileName),
+                FileName = Path.GetDirectoryName(outputSystemRecordFileName),
                 UseShellExecute = true
             };
             Process.Start(processStartInfo);
@@ -111,13 +129,13 @@ namespace Forms_SystemAudioRecord_23
         private void button_RecordMic_Click(object sender, EventArgs e)
         {
             var dialog = new SaveFileDialog();
-            //dialog.Filter = "Wave files | *.wav";
+            dialog.Filter = "Wave files | *.part.";
             if (dialog.ShowDialog() != DialogResult.OK) return;
-            this.outputFileName = dialog.FileName;
+            this.outputMicRecordFileName = dialog.FileName;
             button_RecordMic.Enabled = false;
             button_StopRecordMic.Enabled = true;
             this.micRecordThread = new Thread(new ThreadStart(RecordMicIn));
-            this.threadControl = 1;
+            this.threadMicRecordControl = 1;
             this.micRecordThread.Start();
         }
 
@@ -125,7 +143,7 @@ namespace Forms_SystemAudioRecord_23
         {
             button_StopRecordMic.Enabled = false;
             button_RecordMic.Enabled = true;
-            this.threadControl = 2;
+            this.threadMicRecordControl = 2;
         }
 
         private void listBox_Micro_SelectedIndexChanged(object sender, EventArgs e)
