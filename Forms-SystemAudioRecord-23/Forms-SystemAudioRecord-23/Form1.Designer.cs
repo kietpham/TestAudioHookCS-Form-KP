b317b3a603e8177a295f6c934538cbe002b9fba3
kietpham <kietfriendly@gmail.com>
2025-07-21 09:27:09 +0700

    t

--- Forms-SystemAudioRecord-23/Forms-SystemAudioRecord-23/Form1.Designer.cs
+++ Forms-SystemAudioRecord-23/Forms-SystemAudioRecord-23/Form1.Designer.cs
@@ -34,13 +34,19 @@
             this.listBox_Micro = new System.Windows.Forms.ListBox();
             this.button_StopRecordMic = new System.Windows.Forms.Button();
             this.button_RecordMic = new System.Windows.Forms.Button();
+            this.label_VoskTranscript_System = new System.Windows.Forms.Label();
+            this.label_MSTranscript_System = new System.Windows.Forms.Label();
+            this.label_MSTranscript_Mic = new System.Windows.Forms.Label();
+            this.label_VoskTranscript_Mic = new System.Windows.Forms.Label();
+            this.checkBox_RecordFile_System = new System.Windows.Forms.CheckBox();
+            this.checkBox_RecordFile_Mic = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button_Record
             // 
-            this.button_Record.Location = new System.Drawing.Point(13, 103);
+            this.button_Record.Location = new System.Drawing.Point(368, 38);
             this.button_Record.Name = "button_Record";
-            this.button_Record.Size = new System.Drawing.Size(350, 41);
+            this.button_Record.Size = new System.Drawing.Size(141, 30);
             this.button_Record.TabIndex = 0;
             this.button_Record.Text = "Record Output";
             this.button_Record.UseVisualStyleBackColor = true;
@@ -48,9 +54,9 @@
             // 
             // buttonStop
             // 
-            this.buttonStop.Location = new System.Drawing.Point(13, 150);
+            this.buttonStop.Location = new System.Drawing.Point(368, 67);
             this.buttonStop.Name = "buttonStop";
-            this.buttonStop.Size = new System.Drawing.Size(349, 41);
+            this.buttonStop.Size = new System.Drawing.Size(141, 30);
             this.buttonStop.TabIndex = 1;
             this.buttonStop.Text = "Stop Recording Output";
             this.buttonStop.UseVisualStyleBackColor = true;
@@ -78,9 +84,9 @@
             // 
             // button_StopRecordMic
             // 
-            this.button_StopRecordMic.Location = new System.Drawing.Point(12, 365);
+            this.button_StopRecordMic.Location = new System.Drawing.Point(368, 282);
             this.button_StopRecordMic.Name = "button_StopRecordMic";
-            this.button_StopRecordMic.Size = new System.Drawing.Size(349, 41);
+            this.button_StopRecordMic.Size = new System.Drawing.Size(141, 30);
             this.button_StopRecordMic.TabIndex = 5;
             this.button_StopRecordMic.Text = "Stop Recording Mic";
             this.button_StopRecordMic.UseVisualStyleBackColor = true;
@@ -88,19 +94,81 @@
             // 
             // button_RecordMic
             // 
-            this.button_RecordMic.Location = new System.Drawing.Point(12, 318);
+            this.button_RecordMic.Location = new System.Drawing.Point(368, 254);
             this.button_RecordMic.Name = "button_RecordMic";
-            this.button_RecordMic.Size = new System.Drawing.Size(350, 41);
+            this.button_RecordMic.Size = new System.Drawing.Size(141, 30);
             this.button_RecordMic.TabIndex = 4;
             this.button_RecordMic.Text = "Record Mic";
             this.button_RecordMic.UseVisualStyleBackColor = true;
             this.button_RecordMic.Click += new System.EventHandler(this.button_RecordMic_Click);
             // 
+            // label_VoskTranscript_System
+            // 
+            this.label_VoskTranscript_System.AutoSize = true;
+            this.label_VoskTranscript_System.Location = new System.Drawing.Point(13, 111);
+            this.label_VoskTranscript_System.Name = "label_VoskTranscript_System";
+            this.label_VoskTranscript_System.Size = new System.Drawing.Size(101, 16);
+            this.label_VoskTranscript_System.TabIndex = 6;
+            this.label_VoskTranscript_System.Text = "Vosk Transcript";
+            // 
+            // label_MSTranscript_System
+            // 
+            this.label_MSTranscript_System.AutoSize = true;
+            this.label_MSTranscript_System.Location = new System.Drawing.Point(13, 160);
+            this.label_MSTranscript_System.Name = "label_MSTranscript_System";
+            this.label_MSTranscript_System.Size = new System.Drawing.Size(90, 16);
+            this.label_MSTranscript_System.TabIndex = 7;
+            this.label_MSTranscript_System.Text = "MS Transcript";
+            // 
+            // label_MSTranscript_Mic
+            // 
+            this.label_MSTranscript_Mic.AutoSize = true;
+            this.label_MSTranscript_Mic.Location = new System.Drawing.Point(13, 373);
+            this.label_MSTranscript_Mic.Name = "label_MSTranscript_Mic";
+            this.label_MSTranscript_Mic.Size = new System.Drawing.Size(90, 16);
+            this.label_MSTranscript_Mic.TabIndex = 9;
+            this.label_MSTranscript_Mic.Text = "MS Transcript";
+            // 
+            // label_VoskTranscript_Mic
+            // 
+            this.label_VoskTranscript_Mic.AutoSize = true;
+            this.label_VoskTranscript_Mic.Location = new System.Drawing.Point(13, 324);
+            this.label_VoskTranscript_Mic.Name = "label_VoskTranscript_Mic";
+            this.label_VoskTranscript_Mic.Size = new System.Drawing.Size(101, 16);
+            this.label_VoskTranscript_Mic.TabIndex = 8;
+            this.label_VoskTranscript_Mic.Text = "Vosk Transcript";
+            // 
+            // checkBox_RecordFile_System
+            // 
+            this.checkBox_RecordFile_System.AutoSize = true;
+            this.checkBox_RecordFile_System.Location = new System.Drawing.Point(368, 13);
+            this.checkBox_RecordFile_System.Name = "checkBox_RecordFile_System";
+            this.checkBox_RecordFile_System.Size = new System.Drawing.Size(99, 20);
+            this.checkBox_RecordFile_System.TabIndex = 10;
+            this.checkBox_RecordFile_System.Text = "Record File";
+            this.checkBox_RecordFile_System.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_RecordFile_Mic
+            // 
+            this.checkBox_RecordFile_Mic.AutoSize = true;
+            this.checkBox_RecordFile_Mic.Location = new System.Drawing.Point(368, 228);
+            this.checkBox_RecordFile_Mic.Name = "checkBox_RecordFile_Mic";
+            this.checkBox_RecordFile_Mic.Size = new System.Drawing.Size(99, 20);
+            this.checkBox_RecordFile_Mic.TabIndex = 11;
+            this.checkBox_RecordFile_Mic.Text = "Record File";
+            this.checkBox_RecordFile_Mic.UseVisualStyleBackColor = true;
+            // 
             // FormRecordSpeaker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 419);
+            this.ClientSize = new System.Drawing.Size(857, 443);
+            this.Controls.Add(this.checkBox_RecordFile_Mic);
+            this.Controls.Add(this.checkBox_RecordFile_System);
+            this.Controls.Add(this.label_MSTranscript_Mic);
+            this.Controls.Add(this.label_VoskTranscript_Mic);
+            this.Controls.Add(this.label_MSTranscript_System);
+            this.Controls.Add(this.label_VoskTranscript_System);
             this.Controls.Add(this.button_StopRecordMic);
             this.Controls.Add(this.button_RecordMic);
             this.Controls.Add(this.listBox_Micro);
@@ -110,6 +178,7 @@
             this.Name = "FormRecordSpeaker";
             this.Text = "RecordSpeaker";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -121,6 +190,12 @@
         private System.Windows.Forms.ListBox listBox_Micro;
         private System.Windows.Forms.Button button_StopRecordMic;
         private System.Windows.Forms.Button button_RecordMic;
+        private System.Windows.Forms.Label label_VoskTranscript_System;
+        private System.Windows.Forms.Label label_MSTranscript_System;
+        private System.Windows.Forms.Label label_MSTranscript_Mic;
+        private System.Windows.Forms.Label label_VoskTranscript_Mic;
+        private System.Windows.Forms.CheckBox checkBox_RecordFile_System;
+        private System.Windows.Forms.CheckBox checkBox_RecordFile_Mic;
     }
 }
 
--- Forms-SystemAudioRecord-23/Forms-SystemAudioRecord-23/Form1.cs
+++ Forms-SystemAudioRecord-23/Forms-SystemAudioRecord-23/Form1.cs
@@ -1,25 +1,38 @@
 ﻿using NAudio.Wave;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Management;
+using System.Net.Http;
 using System.Threading;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using Vosk;
 
 
 namespace Forms_SystemAudioRecord_23
 {
     public partial class FormRecordSpeaker : Form
     {
-        private string outputMicRecordFileName;
-        private string outputSystemRecordFileName;
+        public static string labelVoskTranscriptSystemAudio;
+        public static string labelVoskTranscriptMicIn;
+        public static string labelMSTranscriptSystemAudio;
+        public static string labelMSTranscriptMicIn;
+        private string outputMicRecordFileName = "";
+        private string outputSystemRecordFileName = "";
         private WasapiLoopbackCapture capture;
         private WaveInEvent waveIn;
         private Thread micRecordThread;
         private Thread systemAudioRecordThread;
         private int threadMicRecordControl; // 1 start, 2 stop
         private int threadSystemAudioRecordControl; // 1 start, 2 stop
+        public int micSampleRate = 44100;
+        public int systemAudioSampleRate = 16000;
+
+        public Model voskModel = new Model("C:\\GitHub\\vosk-model-small-en-us-0.15");
 
         public FormRecordSpeaker()
         {
@@ -32,33 +45,58 @@ namespace Forms_SystemAudioRecord_23
                 Console.WriteLine(directSoundOutList[i].Description.ToString());
                 listBox_Speakers.Items.Add(directSoundOutList[i].Description);
             }
+            this.label_MSTranscript_Mic.Text = labelMSTranscriptMicIn;
+            this.label_MSTranscript_System.Text = labelMSTranscriptSystemAudio;
+            this.label_VoskTranscript_Mic.Text = labelVoskTranscriptMicIn;
+            this.label_VoskTranscript_System.Text = labelVoskTranscriptSystemAudio;
         }
 
         void RecordMicIn()
         {
+            Console.WriteLine("RecordMicIn");
             int i = 0;
+            var recVosk = new VoskRecognizer(this.voskModel, this.micSampleRate);
             while (threadMicRecordControl == 1 && i < 200)
             {
-                string fileOutputName = this.outputMicRecordFileName + i.ToString() + ".wav";
-                var waveFormat = new WaveFormat(44100, 1);
-                using (var waveFile = new WaveFileWriter(fileOutputName, waveFormat))
+                var waveFormat = new WaveFormat(this.micSampleRate, 1);
+                var byteBuffer = new List<byte>();
+                using (this.waveIn = new WaveInEvent())
                 {
-                    using (this.waveIn = new WaveInEvent())
+                    waveIn.WaveFormat = waveFormat;
+                    waveIn.DataAvailable += (s, e_wi) =>
                     {
-                        waveIn.WaveFormat = waveFormat;
+                        // mic 0 => default , có thể chọn tùy thích
+                        var audioByteArray = new byte[e_wi.BytesRecorded];
+                        Array.Copy(e_wi.Buffer, audioByteArray, e_wi.BytesRecorded);
+                        byteBuffer.AddRange(audioByteArray);
+                    };
+                    waveIn.StartRecording();
+                    Thread.Sleep(5000);
+                    this.waveIn.StopRecording();
 
-                        waveIn.DataAvailable += (s, e_wi) =>
-                        {
-                            // mic 0 => default , có thể chọn tùy thích
-                            waveFile.Write(e_wi.Buffer, 0, e_wi.BytesRecorded);
-                        };
-
-                        waveIn.StartRecording();
-                        Thread.Sleep(5000);
-                        this.waveIn.StopRecording();
-                        if (this.threadMicRecordControl == 2) return;
-                        i++;
+                    // save File
+                    var soundByteArray = byteBuffer.ToArray();
+                    if (checkBox_RecordFile_System.Checked == true)
+                    {
+                        string fileOutputName = this.outputMicRecordFileName + i.ToString() + ".wav";
+                        var waveFile = new WaveFileWriter(fileOutputName, waveFormat);
+                        waveFile.Write(soundByteArray, 0, soundByteArray.Length);
+                        waveFile.Flush();
                     }
+
+                    // Transcript Mic using Vosk
+                    if (recVosk.AcceptWaveform(soundByteArray, soundByteArray.Length))
+                    {
+                        var voskResult = recVosk.Result();
+                        Console.WriteLine("recVosk: " + soundByteArray.Length.ToString() + voskResult);
+                        var voskResultValue = JObject.Parse(voskResult)["text"];
+                        labelVoskTranscriptMicIn += voskResultValue + "|";
+                        Console.WriteLine(voskResultValue);
+                        ThreadHelperClass.SetText(this, label_VoskTranscript_Mic, labelVoskTranscriptMicIn);
+                    }
+
+                    if (this.threadMicRecordControl == 2) return;
+                    i++;
                 }
             }
         }
@@ -66,37 +104,65 @@ namespace Forms_SystemAudioRecord_23
         void RecordSystemAudio()
         {
             int i = 0;
+            var recVosk = new VoskRecognizer(this.voskModel, this.systemAudioSampleRate);
             while (threadSystemAudioRecordControl == 1 && i < 200)
             {
-                string fileOutputName = this.outputSystemRecordFileName + i.ToString() + ".wav";
-                Console.WriteLine(fileOutputName);
-                this.capture = new WasapiLoopbackCapture();
-                var writer = new WaveFileWriter(fileOutputName, capture.WaveFormat);
-                // 2 cách đều dùng được
+                capture = new WasapiLoopbackCapture();
+                capture.WaveFormat = new WaveFormat(this.systemAudioSampleRate, 1);
+                var byteBuffer = new List<byte>();
+                // Capture Async dùng được, nhưng khó khăn khi làm transcript
+                //string fileOutputName = this.outputSystemRecordFileName + i.ToString() + ".wav";
+                //Console.WriteLine(fileOutputName);
+                //var writer = new WaveFileWriter(fileOutputName, capture.WaveFormat);
                 //capture.DataAvailable += async (s, e_a) =>
                 //{
-                //    if (writer != null)
-                //    {
-                //        await writer.WriteAsync(e_a.Buffer, 0, e_a.BytesRecorded);
-                //        await writer.FlushAsync();
+                //    if (writer != null) {
+                //        Console.WriteLine("WasapiLoopbackCapture Buffer BytesRecorded: " + e_a.BytesRecorded);
+                //        if (checkBox_RecordFile_System.Checked == true) {
+                //            await writer.WriteAsync(e_a.Buffer, 0, e_a.BytesRecorded);
+                //            await writer.FlushAsync();
+                //        }
                 //    }
                 //};
+                // Capture không Async, dùng byte làm transcript tốt hơn
                 capture.DataAvailable += (s, e_a) =>
                 {
-                    if (writer != null)
-                    {
-                        writer.WriteAsync(e_a.Buffer, 0, e_a.BytesRecorded);
-                        writer.FlushAsync();
-                    }
+                    var audioByteArray = new byte[e_a.BytesRecorded];
+                    Array.Copy(e_a.Buffer, audioByteArray, e_a.BytesRecorded);
+                    byteBuffer.AddRange(audioByteArray);
                 };
+                capture.RecordingStopped += (s, e) => { Console.WriteLine("Capture Recording Stopped"); };
+
                 capture.StartRecording();
                 Thread.Sleep(5000);
                 this.capture.StopRecording();
+
+                var soundByteArray = byteBuffer.ToArray();
+
+                // Save file
+                if (checkBox_RecordFile_System.Checked == true)
+                {
+                    string fileOutputName = this.outputSystemRecordFileName + i.ToString() + ".wav";
+                    Console.WriteLine(fileOutputName);
+                    var writer = new WaveFileWriter(fileOutputName, capture.WaveFormat);
+                    writer.Write(soundByteArray, 0, soundByteArray.Length);
+                    writer.Flush();
+                }
+                // Transcript System Audio using Vosk
+                if (recVosk.AcceptWaveform(soundByteArray, soundByteArray.Length))
+                {
+                    var voskResult = recVosk.Result();
+                    Console.WriteLine("recVosk: " + soundByteArray.Length.ToString() + voskResult);
+                    var voskResultValue = JObject.Parse(voskResult)["text"];
+                    labelVoskTranscriptSystemAudio += voskResult + "|";
+                    Console.WriteLine(voskResultValue);
+                    ThreadHelperClass.SetText(this, label_VoskTranscript_System, labelVoskTranscriptSystemAudio);
+                }
+
                 if (this.threadSystemAudioRecordControl == 2)
                 {
                     Console.WriteLine("RecordSystemAudio End");
-                    writer.Dispose();
-                    capture.Dispose();
+                    this.capture.Dispose();
                     return;
                 }
                 i++;
@@ -111,9 +177,12 @@ namespace Forms_SystemAudioRecord_23
         private void button_Record_Click(object sender, EventArgs e)
         {
             var dialog = new SaveFileDialog();
-            dialog.Filter = "Wave files | *.part.";
-            if (dialog.ShowDialog() != DialogResult.OK) return;
-            outputSystemRecordFileName = dialog.FileName;
+            if (this.checkBox_RecordFile_System.Checked == true)
+            {
+                dialog.Filter = "Wave files | *.part.";
+                if (dialog.ShowDialog() != DialogResult.OK) return;
+                outputSystemRecordFileName = dialog.FileName;
+            }
             button_Record.Enabled = false;
             buttonStop.Enabled = true;
             this.threadSystemAudioRecordControl = 1;
@@ -138,9 +207,12 @@ namespace Forms_SystemAudioRecord_23
         private void button_RecordMic_Click(object sender, EventArgs e)
         {
             var dialog = new SaveFileDialog();
-            dialog.Filter = "Wave files | *.part.";
-            if (dialog.ShowDialog() != DialogResult.OK) return;
-            this.outputMicRecordFileName = dialog.FileName;
+            if (checkBox_RecordFile_Mic.Checked == true)
+            {
+                dialog.Filter = "Wave files | *.part.";
+                if (dialog.ShowDialog() != DialogResult.OK) return;
+                this.outputMicRecordFileName = dialog.FileName;
+            }
             button_RecordMic.Enabled = false;
             button_StopRecordMic.Enabled = true;
             this.micRecordThread = new Thread(new ThreadStart(RecordMicIn));
@@ -158,6 +230,23 @@ namespace Forms_SystemAudioRecord_23
         private void listBox_Micro_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+    }
+
+    public static class ThreadHelperClass
+    {
+        delegate void SetTextCallback(Form f, Control ctrl, string text);
+        public static void SetText(Form form, Control ctrl, string text)
+        {
+            if (ctrl.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                form.Invoke(d, new object[] { form, ctrl, text });
+            }
+            else
+            {
+                ctrl.Text = text;
+            }
         }
     }
 }
